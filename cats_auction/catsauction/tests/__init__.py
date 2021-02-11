fdba38f78cedb753c9f1889d880c5cd0325e396d
Ruslan Zaporojets <ruzzzua@gmail.com>
2021-02-11 07:42:43 +0200

    Removed catsauction, mblog, added zero_to_end

--- cats_auction/catsauction/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.environ.setdefault('CATSAUCTION_CONF', 'test')
--- tasks_python/tictactoy_test.py
+++ tasks_python/tictactoy_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tictactoy.tictactoy import TicTacToy, Player
+from tictactoy import TicTacToy, Player
 
 
 @pytest.fixture
